3cc6a698b1c1ac125092de1fd8c54772ed79107b
KH0308 <khairulikwan262@gmail.com>
2022-03-10 23:12:52 +0800

    commit new

---
+++ samples/src/samples/demo/location.java
@@ -0,0 +1,14 @@
+package samples.demo;
+
+public class location {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
